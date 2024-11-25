317736fa062b94f1ac0610cc59177c7df9c9bebf
binschocolate <hisbulletp@gmail.com>
2024-11-25 21:05:52 +0900

    기초ui

---
+++ HomeCamCheck/HomeCamCheck/HomeCamCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeCamCheckApp.swift
+//  HomeCamCheck
+//
+//  Created by 박현빈 on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct HomeCamCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
